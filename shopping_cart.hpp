bd6e3a616d39c4d16a7bec8fa32d277677496ccf
Saifeldin Mohamed Ahmed <127222866+SaiffMoh@users.noreply.github.com>
2023-11-21 13:46:04 +0200

    Update shopping_cart.hpp

--- shopping_cart.hpp
+++ shopping_cart.hpp
@@ -2,31 +2,18 @@
 using namespace std;
 
 class MaxHeap {
-private:
-    static const int MAX_SIZE = 100;
-    int* heap;  
-    int size;
+
 public: 
 
 MaxHeap();
 ~MaxHeap();
-bool isEmpty() const {
-        return size == 0;
-    }
+bool isEmpty();
 int getMax();
 void insertItem();
 void shiftUp();
 void ShiftDown();
 int extractMax();
-void deleteAll(){
-    int* tempHeap = new int[MAX_SIZE];
-    for (int i = 0; i < size; ++i) {
-        tempHeap[i] = heap[i];
-        }
-    delete[] heap;
-    heap = tempHeap;
-    size = 0;
-}
+void deleteAll();
 ~MaxHeap();
 
 
