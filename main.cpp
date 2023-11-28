0cdb75c62039e9730ffe4218b59b9d4ea3f494fe
ahmed-lotfy-abousalem <ahmedlotfy122@yahoo.com>
2023-11-28 19:10:43 +0200

    new main to test functions

--- main.cpp
+++ main.cpp
@@ -6,28 +6,44 @@ using namespace std;
 
 int main() 
 {
-    MaxHeap productCatalog(100); 
+   MaxHeap productCatalog(10);
 
     
-    Product prod1("Product1", "Description1", 10, 50);
-    Product prod2("Product2", "Description2", 20, 30);
-    Product prod3("Product3", "Description3", 15, 25);
+    Product product1("Product1", "Description1", 10, 50);
+    Product product2("Product2", "Description2", 20, 30);
+    Product product3("Product3", "Description3", 15, 25);
 
-    productCatalog.insertProduct(prod1);
-    productCatalog.insertProduct(prod2);
-    productCatalog.insertProduct(prod3);
+    productCatalog.addToCart(product1);
+    productCatalog.addToCart(product2);
+    productCatalog.addToCart(product3);
 
     
+    cout << "Product Catalog:\n";
     productCatalog.displayProductCatalog();
 
     
-    MaxHeap shoppingCart(50); 
+    MaxHeap shoppingCart(10);
 
     
-    shoppingCart.addToCart(productCatalog.extractMaxProduct());
-    shoppingCart.addToCart(productCatalog.extractMaxProduct());
+    shoppingCart.addProductToCart("Product1", productCatalog);
+    shoppingCart.addProductToCart("Product2", productCatalog);
 
     
+    cout << "Shopping Cart:\n";
+    shoppingCart.displayShoppingCart();
+
+    
+    shoppingCart.removeFromCart("Product1");
+
+   
+    cout << "Updated Shopping Cart:\n";
+    shoppingCart.displayShoppingCart();
+
+    
+    shoppingCart.updateQuantity("Product2", 5);
+
+    
+    cout << "Shopping Cart After Quantity Update:\n";
     shoppingCart.displayShoppingCart();
 
     return 0;
